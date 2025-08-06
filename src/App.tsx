b7e4b7de45faecc68511fb96c948fc1c4407930f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-08-06 16:40:26 +0530

    feat: create basic input page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
-    <>
+    <div className="flex items-center justify-center h-screen">
       <div className="grid w-full max-w-sm items-center gap-3">
-        <Label htmlFor="picture">Picture</Label>
-        <Input id="picture" type="file" />
+        <Input id="Report" type="file" />
+        <Button className="w-1/3 space-y-6" type="submit">Submit</Button>
       </div>
-    </>
+    </div>
   )
 }
 
